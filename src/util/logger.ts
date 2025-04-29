efb4b0a5334f6fbdf5bbca11baf5cdf5cddb4057
Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>
2025-04-29 14:35:20 +0200

    feat: use JSON log format (#51)
    * feat: switch to json logging format
    
    Signed-off-by: Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>
    
    * fix: lint
    
    Signed-off-by: Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>
    
    ---------
    
    Signed-off-by: Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,17 +1,7 @@
 import { createLogger, transports, format } from 'winston';
 
-const logFormat = format.printf(
-  ({ level, message, timestamp, ...metadata }) => {
-    let msg = `${timestamp} [${level}] : ${message} `;
-    if (metadata && Object.keys(metadata).length > 0) {
-      msg += JSON.stringify(metadata);
-    }
-    return msg;
-  }
-);
-
 export default createLogger({
   level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL.toLowerCase() : 'info',
-  format: format.combine(format.colorize(), format.timestamp(), logFormat),
+  format: format.combine(format.timestamp(), format.json()),
   transports: [new transports.Console()]
 });
